8c8095fd0de1c8a71f700b1e252c41ca85295839
Colin Stevens <colinstevens06@gmail.com>
2020-02-16 21:21:05 -0500

    updates foures and the first get is working

--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,27 @@
-const db = require("./models")
+// const router = require("express").Router();
+const router = require("express").Router();
+const db = require("../models")
+// need to map out CRUD actions
+
+// CREATE/post
+router.post("/api/workouts", ({ body }, res) => {
+  console.log(body)
+})
+
+// READ/get
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({}).then(dbCardio => {
+    res.json(dbCardio)
+  }).catch(err => {
+    res.json(err)
+  })
+})
+
+// UPDATE/PUT
+
+
+// DELETE/REMOVE
+
+
+
+module.exports = router
