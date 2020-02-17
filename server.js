9263907f51846a0933d24ff5b38e8207936bbf44
Colin Stevens <colinstevens06@gmail.com>
2020-02-16 21:21:15 -0500

    updated server file

--- server.js
+++ server.js
@@ -12,15 +12,32 @@ app.use(logger("dev"))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use(express.statis("public"))
+app.use(express.static("public"))
 
 // connect to the database
-mongoose.connect(process.env.MONGDB_URI || "mongodb://localhost/populate", { useNewUrlParser: true })
+mongoose.connect(process.env.MONGDB_URI || "mongodb://localhost/workout", { useNewUrlParser: true })
+
+// app.use(require("./seeders/seed"))
 
 // importing my routes
+// const router = require("express").Router();
+const db = require("./models")
 
+// need to map out CRUD actions
 
+// CREATE/post
+app.post("/api/workouts", ({ body }, res) => {
+  console.log(body)
+})
 
+// READ/get
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({}).then(dbCardio => {
+    res.json(dbCardio)
+  }).catch(err => {
+    res.json(err)
+  })
+})
 // server is listening
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
